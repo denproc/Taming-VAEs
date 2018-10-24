166475d451390368cff9a2f2ce7c6b4341d530f0
Anna Kuzina <ftm.rk1@gmail.com>
2018-10-25 00:13:09 +0300

    Correct the baguli in geco

--- GECO.py
+++ GECO.py
@@ -4,82 +4,92 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as data_utils
 import torch
+import time
+from IPython import display
+import matplotlib.pyplot as plt
+from tqdm import tqdm_notebook
+
 
 def KL_divergence(mu, logsigma):
     return - 0.5 * torch.sum(1 + 2 * logsigma - mu.pow(2) - logsigma.exp().pow(2), dim=1)
 
 def RE(x, mu, tol):
-    return torch.sum(torch.pow(mu - x, 2)) - tol**2
+    return torch.sum(torch.pow(mu - x, 2), dim = 1) - tol**2
 
 
 def train_geco(model, opt, scheduler, train_loader, valid_loader, 
                lambd_init = torch.FloatTensor([1]), 
                KL_divergence = KL_divergence, 
                constraint_f = RE, num_epochs=20, 
-               lbd_step = 5, alpha = 0.99, visualize = True, device = 'cuda'):
+               lbd_step = 100, alpha = 0.99, visualize = True, 
+               device = 'cpu', tol = 1):
     
     model.to(device)
     
-    train_hist = {'loss':[0], 'reconstr':[0], 'KL':[0]}
-    valid_hist = {'loss':[0], 'reconstr':[0], 'KL':[0]}
+    train_hist = {'loss':[], 'reconstr':[], 'KL':[]}
+    valid_hist = {'loss':[], 'reconstr':[], 'KL':[]}
+    lambd_hist = []
     
-    n_tr_batches = len(train_loader)//train_loader.batch_size
-    n_te_natches = len(valid_loader)//valid_loader.batch_size
-    
-    lambd = lambd_init
+    lambd = lambd_init.to(device)
+    iter_num = 0
     
     for epoch in range(num_epochs):
         # a full pass over the training data:
         start_time = time.time()
-        model.train(True)
-        for (X_batch, y_batch) in train_loader:
-            X_batch.to(device)
-    
-            reconstruction_mu, reconstruction_logsigma, latent_mu, latent_logsigma = model(X_batch)
         
-            constraint = constraint_f(X_batch, reconstruction_mu, tol = 0.1)
-            KL_div = KL_divergence(latent_mu, latent_logsigma)
+        model.train(True)
+        train_hist['loss'].append(0)
+        train_hist['reconstr'].append(0)
+        train_hist['KL'].append(0)
+        
+        for (X_batch, y_batch) in train_loader:
+            X_batch = X_batch.reshape(train_loader.batch_size, -1).to(device)
             
-            if epoch == 0:
-                constrain_ma = - constraint
-            else:
-                constrain_ma = alpha * constrain_ma.detach() - (1 - alpha) * constraint
-
+            reconstruction_mu, reconstruction_logsigma, latent_mu, latent_logsigma = model(X_batch)
+            constraint = torch.mean(constraint_f(X_batch, reconstruction_mu, tol = tol))
+            KL_div = torch.mean(KL_divergence(latent_mu, latent_logsigma))
             loss = KL_div + lambd * constraint
+#             loss.backward(retain_graph = True)
             loss.backward()
             opt.step()
             opt.zero_grad()
             
-            if epoch % lbd_step == 0:
-                lambd *= torch.exp(constrain_ma) 
-
-            train_hist['loss'][-1] += loss.data.cpu().numpy()[0]
-            train_hist['reconstr'][-1] += constraint.data.cpu().numpy()[0]
-            train_hist['KL'][-1] += KL_div.data.cpu().numpy()[0]
+            with torch.no_grad():
+                if epoch == 0 and iter_num == 0:
+                    constrain_ma = constraint
+                else:
+                    constrain_ma = alpha * constrain_ma.detach_() + (1 - alpha) * constraint
+                if iter_num % lbd_step == 0:
+#                     print(torch.exp(constrain_ma), lambd)
+                    lambd *= torch.clamp(torch.exp(constrain_ma), 0.9, 1.1)
+                                        
+            train_hist['loss'][-1] += loss.data.cpu().numpy()[0]/len(train_loader)
+            train_hist['reconstr'][-1] += constraint.data.cpu().numpy()/len(train_loader)
+            train_hist['KL'][-1] += KL_div.data.cpu().numpy()/len(train_loader)                                        
+            iter_num += 1
+        lambd_hist.append(lambd.data.cpu().numpy()[0]) 
 
            
         model.train(False)
+        valid_hist['loss'].append(0)
+        valid_hist['reconstr'].append(0)
+        valid_hist['KL'].append(0)
         with torch.no_grad():
             for (X_batch, y_batch) in valid_loader:
-                X_batch.to(device)
+                X_batch = X_batch.reshape(train_loader.batch_size, -1).to(device)
                 reconstruction_mu, reconstruction_logsigma, latent_mu, latent_logsigma = model(X_batch)
 
-                constraint = constraint_f(X_batch, reconstruction_mu, tol = 0.1)
-                KL_div = KL_divergence(latent_mu, latent_logsigma)
+                constraint = torch.mean(constraint_f(X_batch, reconstruction_mu, tol = tol))
+                KL_div = torch.mean(KL_divergence(latent_mu, latent_logsigma))
 
-                valid_hist['loss'][-1] += loss.data.cpu().numpy()[0]
-                valid_hist['reconstr'][-1] += constraint.data.cpu().numpy()[0]
-                valid_hist['KL'][-1] += KL_div.data.cpu().numpy()[0]
+                valid_hist['loss'][-1] += loss.data.cpu().numpy()[0]/len(valid_loader)
+                valid_hist['reconstr'][-1] += constraint.data.cpu().numpy()/len(valid_loader)
+                valid_hist['KL'][-1] += KL_div.data.cpu().numpy()/len(valid_loader)
         
-        train_hist['loss'][-1] /= n_tr_batches
-        valid_hist['loss'][-1] /= n_te_batches
-        train_hist['reconstr'] /= n_tr_batches
-        valid_hist['reconstr'] /= n_te_batches
-        train_hist['KL'] /= n_tr_batches
-        valid_hist['KL'] /= n_te_batches
 
         # update lr
-        scheduler.step(valid_hist['loss'][-1])
+        if scheduler is not None:
+            scheduler.step(valid_hist['loss'][-1])
         # stop
         if opt.param_groups[0]['lr'] <= 1e-6:
             break
@@ -88,7 +98,7 @@ def train_geco(model, opt, scheduler, train_loader, valid_loader,
         # visualization of training
         if visualize:
             display.clear_output(wait=True)
-            fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(16, 5))
+            fig, ax = plt.subplots(ncols=4, nrows=1, figsize=(16, 4))
 
             ax[0].plot(train_hist['loss'], label = 'train')
             ax[0].plot(valid_hist['loss'], label = 'test')
@@ -104,6 +114,10 @@ def train_geco(model, opt, scheduler, train_loader, valid_loader,
             ax[2].plot(valid_hist['KL'], label = 'test')
             ax[2].legend()
             ax[2].set_title("KL divergence")
+            
+            ax[3].plot(lambd_hist)
+            ax[3].set_title("Lambda")
+            plt.show()
 
 
         print("Epoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
